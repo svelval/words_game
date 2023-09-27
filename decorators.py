8a2d18a7dc86dc90771b0eb624acab5c0dc82925
svelval <93996330+svelval@users.noreply.github.com>
2023-09-27 14:21:09 +0500

    'database_errors_handler' decorator temporary separated to 'async_wrapper' & 'sync_wrapper'

--- decorators.py
+++ decorators.py
@@ -6,6 +6,27 @@ from database_exceptions import ConnectionPoolCannotBeCreated, ConnectionPoolDoe
 
 
 def database_errors_handler(fun):
+    async def async_wrapper(*args, **kwargs):
+        try:
+            return await fun(*args, **kwargs)
+        except (AttributeError, NameError):
+            raise ConnectionPoolDoesNotExist('Connection pool does not exist')
+        except OperationalError as ex:
+            raise ConnectionPoolCannotBeCreated(f'Connection pool cannot be created: {ex}')
+        except Exception as other_ex:
+            raise InternalDatabaseError(f'Internal database error: {other_ex}')
+
+    def sync_wrapper(*args, **kwargs):
+        try:
+            return fun(*args, **kwargs)
+        except (AttributeError, NameError):
+            raise ConnectionPoolDoesNotExist('Connection pool does not exist')
+        except OperationalError as ex:
+            raise ConnectionPoolCannotBeCreated(f'Connection pool cannot be created: {ex}')
+        except Exception as other_ex:
+            raise InternalDatabaseError(f'Internal database error: {other_ex}')
+
+
     async def wrapper(*args, **kwargs):
         try:
             if iscoroutinefunction(fun):
@@ -18,4 +39,7 @@ def database_errors_handler(fun):
             raise ConnectionPoolCannotBeCreated(f'Connection pool cannot be created: {ex}')
         except Exception as other_ex:
             raise InternalDatabaseError(f'Internal database error: {other_ex}')
-    return wrapper
+    if iscoroutinefunction(fun):
+        return async_wrapper
+    else:
+        return sync_wrapper
